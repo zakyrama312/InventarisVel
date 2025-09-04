9bc7a71deb992a79520a72fc39d1b6632e94add9
zakyrama312 <zakyrama312@gmail.com>
2025-09-04 10:57:41 +0700

    revisi name ventera

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Inventaris Vel</title>
+    <title>Ventera</title>
     <link rel="shortcut icon" type="image/png" href="{{ asset('/')}}assets/images/logos/favicon.png" />
     <link rel="stylesheet" href="{{ asset('/')}}assets/css/styles.min.css" />
     <link rel="stylesheet" href="{{ asset('/')}}assets/css/mystyle.css" />
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,7 +4,7 @@
         <div class="brand-logo d-flex align-items-center justify-content-between">
             <a href="./index.html" class="text-nowrap logo-img">
                 {{-- <img src="{{ asset('/')}}assets/images/logos/dark-logo.svg" width="180" alt="" /> --}}
-                <img src="{{ asset('/')}}assets/images/logos/inv.png" width="180" alt="">
+                <img src="{{ asset('/')}}assets/images/logos/ventera.png" width="180" alt="">
                 {{-- <h3 class="brand">Inventaris Vel</h3> --}}
             </a>
             <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
